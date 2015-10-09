312037522606231522d4e73a965be9c49cb4143f
Mike Gaffney <gaffo@justin.tv>
2015-10-09 14:09:28 -0700

    Adding indenting

--- vpcgraph/main.go
+++ vpcgraph/main.go
@@ -15,42 +15,49 @@ import (
 )
 
 type Printable interface {
-	Print(io.Writer)
+	Print(int, io.Writer)
 }
 
 type Graph struct {
 	Name     string
+	Label    string
 	Children []Printable
 }
 
-func (self *Graph) Print(out io.Writer) {
-	fmt.Fprintf(out, "graph %s {\n", self.Name)
+func (self *Graph) Print(depth int, out io.Writer) {
+	prefix := Indent(depth)
+	fmt.Fprintf(out, "%sgraph %s {\n", prefix, self.Name)
 
 	for _, child := range self.Children {
-		child.Print(out)
+		child.Print(depth+1, out)
 	}
-	fmt.Fprintf(out, "label=\"%s\";\n", self.Name)
-	fmt.Fprint(out, "}\n")
+	prefix = Indent(depth + 1)
+	fmt.Fprintf(out, "%slabel=\"%s\";\n", prefix, self.Label)
+	prefix = Indent(depth)
+	fmt.Fprintf(out, "%s}\n", prefix)
 }
 
 type Subgraph struct {
 	Name     string
 	Children []Printable
+	Label    string
 	Nodes    []string
 }
 
-func (self Subgraph) Print(out io.Writer) {
-	fmt.Fprintf(out, "subgraph cluster_%s {\n", self.Name)
-	fmt.Fprintf(out, "color=lightgrey;\n")
-	fmt.Fprintf(out, "node [style=filled,color=lightgrey];\n")
+func (self Subgraph) Print(depth int, out io.Writer) {
+	prefix := Indent(depth)
+	fmt.Fprintf(out, "%ssubgraph cluster_%s {\n", prefix, self.Name)
+	prefix = Indent(depth + 1)
+	fmt.Fprintf(out, "%scolor=lightgrey;\n", prefix)
+	fmt.Fprintf(out, "%snode [style=filled,color=lightgrey];\n", prefix)
 
-	fmt.Fprintf(out, "label = \"%s\";\n", self.Name)
+	fmt.Fprintf(out, "%slabel = \"%s\";\n", prefix, self.Label)
 
 	for _, node := range self.Nodes {
-		fmt.Fprintf(out, "%s;\n", node)
+		fmt.Fprintf(out, "%s%s;\n", prefix, node)
 	}
-
-	fmt.Fprint(out, "}\n")
+	prefix = Indent(depth)
+	fmt.Fprintf(out, "%s}\n", prefix)
 }
 
 func FindTag(name string, tags []*ec2.Tag) *ec2.Tag {
@@ -71,6 +78,15 @@ func DotSafe(str string) string {
 	return strings.Replace(str, "-", "_", -1)
 }
 
+func Indent(depth int) string {
+	depth = depth * 2
+	s := ""
+	for i := 0; i < depth; i++ {
+		s += " "
+	}
+	return s
+}
+
 func main() {
 	fmt.Println("Running")
 	svc := ec2.New(&aws.Config{Region: aws.String("us-west-2")})
@@ -89,6 +105,7 @@ func main() {
 	defer file.Close()
 
 	vpc := resp.Vpcs[0]
+	fmt.Println(vpc)
 
 	vpcName := FindNameTag(vpc.Tags)
 	fmt.Println(vpcName)
@@ -99,12 +116,13 @@ func main() {
 
 	subnet := Subgraph{
 		Name:  vpcName,
-		Nodes: []string{"a1", "a2"},
+		Label: fmt.Sprintf("%s: %s", vpcName, DotSafe(*vpc.CidrBlock)),
+		Nodes: []string{"_"},
 	}
 
 	graph.Children = append(graph.Children, subnet)
 
-	graph.Print(file)
+	graph.Print(0, file)
 
 	exec.Command("dot", "-Tpng", "-o", "vpc.png", "vpc.dot").Start()
 	exec.Command("open", "vpc.png").Start()
